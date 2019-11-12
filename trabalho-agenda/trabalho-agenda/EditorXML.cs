f2b4d827195359cb5a0197dc4b9639216ece3786
otavio-augusto <ottoaugs@gmail.com>
2019-11-12 17:00:17 -0300

    Serializador agora utiliza XElements, mais rapido e simples.

--- trabalho-agenda/trabalho-agenda/EditorXML.cs
+++ trabalho-agenda/trabalho-agenda/EditorXML.cs
@@ -4,44 +4,58 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 using System.Xml.Serialization;
+using System.Xml.XPath;
 
 namespace trabalho_agenda
 {
     class EditorXML<T>
     {
-        public void serializar(List<T> lista, string arquivo)
+        public void serializar(List<T> lista, string arquivo, bool append = true)
         {
-            lista.AddRange(deserializar(arquivo));
             criar(arquivo);
-            TextWriter writer = new StreamWriter(arquivo);
             XmlSerializer serializer = new XmlSerializer(lista.GetType());
-            serializer.Serialize(writer, lista);
-            writer.Close();
+            XElement Raiz = XElement.Load(arquivo);
+            XElement xElement;
+            using (var stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, lista);
+                stream.Position = 0;
+
+                using (XmlReader reader = XmlReader.Create(stream))
+                {
+                    xElement = XElement.Load(reader);
+                }
+            }
+            Raiz.Add(xElement.Elements());
+            Raiz.Save(arquivo);
         }
 
         public List<T> deserializar(string arquivo)
         {
             criar(arquivo);
             List<T> lista = new List<T>();
-            FileStream fileStream = new FileStream(arquivo, FileMode.Open);
-            fileStream.Position = 0;
+            FileStream fileStream = new FileStream(arquivo, FileMode.OpenOrCreate);
             XmlSerializer serializer = new XmlSerializer(lista.GetType());
             try
             {
                 lista = (List<T>)serializer.Deserialize(fileStream);
             }
-            catch (Exception)
+            catch (System.InvalidOperationException)// XML Vazio
             {
                 lista = new List<T>();
             }
             fileStream.Close();
             return lista;
         }
+
+
         public void criar(string arquivo)
         {
             if (!File.Exists(arquivo))
-                File.Create(arquivo);
+                new XDocument(new XElement("root")).Save(arquivo);
         }
     }
 }
