9c2e3930fff85d2e2ff683f223263bf3a8e0194a
Luiz Guilherme <lacerbi@sga.pucminas.br>
2019-11-18 19:19:24 -0300

    Revert "Disney"
    This reverts commit 80da3f0f5f78dfa077b3eaeafd87ecdfc0d553c9.

--- trabalho-agenda/Form1.Designer.cs
+++ trabalho-agenda/Form1.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cadastrarPacienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -35,6 +38,32 @@
             this.cadastrarConsultaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // menuStrip1
             // 
@@ -59,21 +88,21 @@
             // cadastrarPacienteToolStripMenuItem
             // 
             this.cadastrarPacienteToolStripMenuItem.Name = "cadastrarPacienteToolStripMenuItem";
-            this.cadastrarPacienteToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.cadastrarPacienteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.cadastrarPacienteToolStripMenuItem.Text = "Cadastrar paciente";
             this.cadastrarPacienteToolStripMenuItem.Click += new System.EventHandler(this.cadastrarPacienteToolStripMenuItem_Click);
             // 
             // cadastrarMédicoToolStripMenuItem
             // 
             this.cadastrarMédicoToolStripMenuItem.Name = "cadastrarMédicoToolStripMenuItem";
-            this.cadastrarMédicoToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.cadastrarMédicoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.cadastrarMédicoToolStripMenuItem.Text = "Cadastrar médico";
             this.cadastrarMédicoToolStripMenuItem.Click += new System.EventHandler(this.cadastrarMédicoToolStripMenuItem_Click);
             // 
             // cadastrarConsultaToolStripMenuItem
             // 
             this.cadastrarConsultaToolStripMenuItem.Name = "cadastrarConsultaToolStripMenuItem";
-            this.cadastrarConsultaToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.cadastrarConsultaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.cadastrarConsultaToolStripMenuItem.Text = "Cadastrar consulta";
             this.cadastrarConsultaToolStripMenuItem.Click += new System.EventHandler(this.cadastrarConsultaToolStripMenuItem_Click);
             // 
@@ -82,6 +111,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -94,6 +126,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem cadastrarPacienteToolStripMenuItem;
