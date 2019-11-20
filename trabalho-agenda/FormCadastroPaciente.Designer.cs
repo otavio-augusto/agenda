ea563db6c438ff21da956a6e672dc2dd147ea94a
Luiz Guilherme <lacerbi@sga.pucminas.br>
2019-11-20 19:06:55 -0300

    Função do button Cadastrar Medico/Paciente adicionado

--- trabalho-agenda/FormCadastroPaciente.Designer.cs
+++ trabalho-agenda/FormCadastroPaciente.Designer.cs
@@ -28,15 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblCEP = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonCadastrar = new System.Windows.Forms.Button();
+            this.dtDataNasc = new System.Windows.Forms.DateTimePicker();
+            this.selectTipoSang = new System.Windows.Forms.ComboBox();
+            this.mskdCPF = new System.Windows.Forms.MaskedTextBox();
             this.lblCPF = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textTelefone = new System.Windows.Forms.TextBox();
+            this.textNome = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.lblTipoSang = new System.Windows.Forms.Label();
             this.lblMail = new System.Windows.Forms.Label();
@@ -54,27 +55,21 @@
             this.lblCidade = new System.Windows.Forms.Label();
             this.lblBairro = new System.Windows.Forms.Label();
             this.lblRua = new System.Windows.Forms.Label();
+            this.lblCEP = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblCEP
-            // 
-            this.lblCEP.AutoSize = true;
-            this.lblCEP.Location = new System.Drawing.Point(7, 205);
-            this.lblCEP.Name = "lblCEP";
-            this.lblCEP.Size = new System.Drawing.Size(38, 16);
-            this.lblCEP.TabIndex = 0;
-            this.lblCEP.Text = "CEP:";
-            // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Controls.Add(this.maskedTextBox2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.buttonCadastrar);
+            this.groupBox1.Controls.Add(this.dtDataNasc);
+            this.groupBox1.Controls.Add(this.selectTipoSang);
+            this.groupBox1.Controls.Add(this.mskdCPF);
             this.groupBox1.Controls.Add(this.lblCPF);
-            this.groupBox1.Controls.Add(this.textBox3);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textEmail);
+            this.groupBox1.Controls.Add(this.textTelefone);
+            this.groupBox1.Controls.Add(this.textNome);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.lblTipoSang);
             this.groupBox1.Controls.Add(this.lblMail);
@@ -94,24 +89,45 @@
             this.groupBox1.Controls.Add(this.lblRua);
             this.groupBox1.Controls.Add(this.lblCEP);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(391, 403);
-            this.groupBox1.TabIndex = 1;
+            this.groupBox1.Size = new System.Drawing.Size(391, 435);
+            this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             // 
-            // dateTimePicker1
+            // button2
             // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(13, 172);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(126, 22);
-            this.dateTimePicker1.TabIndex = 26;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(298, 396);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 23);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "Alterar";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // buttonCadastrar
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.buttonCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonCadastrar.Location = new System.Drawing.Point(198, 396);
+            this.buttonCadastrar.Name = "buttonCadastrar";
+            this.buttonCadastrar.Size = new System.Drawing.Size(89, 23);
+            this.buttonCadastrar.TabIndex = 27;
+            this.buttonCadastrar.Text = "Cadastrar";
+            this.buttonCadastrar.UseVisualStyleBackColor = true;
+            this.buttonCadastrar.Click += new System.EventHandler(this.buttonCadastrar_Click);
+            // 
+            // dtDataNasc
+            // 
+            this.dtDataNasc.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtDataNasc.Location = new System.Drawing.Point(13, 172);
+            this.dtDataNasc.Name = "dtDataNasc";
+            this.dtDataNasc.Size = new System.Drawing.Size(126, 22);
+            this.dtDataNasc.TabIndex = 26;
+            // 
+            // selectTipoSang
+            // 
+            this.selectTipoSang.FormattingEnabled = true;
+            this.selectTipoSang.Items.AddRange(new object[] {
             "A+",
             "B+",
             "O+",
@@ -120,18 +136,18 @@
             "B-",
             "O-",
             "AB-"});
-            this.comboBox1.Location = new System.Drawing.Point(156, 170);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(93, 24);
-            this.comboBox1.TabIndex = 25;
+            this.selectTipoSang.Location = new System.Drawing.Point(156, 170);
+            this.selectTipoSang.Name = "selectTipoSang";
+            this.selectTipoSang.Size = new System.Drawing.Size(93, 24);
+            this.selectTipoSang.TabIndex = 25;
             // 
-            // maskedTextBox2
+            // mskdCPF
             // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(267, 171);
-            this.maskedTextBox2.Mask = "000,000,000-00";
-            this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(110, 22);
-            this.maskedTextBox2.TabIndex = 24;
+            this.mskdCPF.Location = new System.Drawing.Point(267, 171);
+            this.mskdCPF.Mask = "000,000,000-00";
+            this.mskdCPF.Name = "mskdCPF";
+            this.mskdCPF.Size = new System.Drawing.Size(110, 22);
+            this.mskdCPF.TabIndex = 24;
             // 
             // lblCPF
             // 
@@ -142,26 +158,26 @@
             this.lblCPF.TabIndex = 23;
             this.lblCPF.Text = "CPF:";
             // 
-            // textBox3
+            // textEmail
             // 
-            this.textBox3.Location = new System.Drawing.Point(13, 126);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(364, 22);
-            this.textBox3.TabIndex = 20;
+            this.textEmail.Location = new System.Drawing.Point(13, 126);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(364, 22);
+            this.textEmail.TabIndex = 20;
             // 
-            // textBox2
+            // textTelefone
             // 
-            this.textBox2.Location = new System.Drawing.Point(13, 82);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(364, 22);
-            this.textBox2.TabIndex = 19;
+            this.textTelefone.Location = new System.Drawing.Point(13, 82);
+            this.textTelefone.Name = "textTelefone";
+            this.textTelefone.Size = new System.Drawing.Size(364, 22);
+            this.textTelefone.TabIndex = 19;
             // 
-            // textBox1
+            // textNome
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 38);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(364, 22);
-            this.textBox1.TabIndex = 18;
+            this.textNome.Location = new System.Drawing.Point(13, 38);
+            this.textNome.Name = "textNome";
+            this.textNome.Size = new System.Drawing.Size(364, 22);
+            this.textNome.TabIndex = 18;
             // 
             // label1
             // 
@@ -270,7 +286,6 @@
             this.btnConsultar.TabIndex = 5;
             this.btnConsultar.Text = "Consultar";
             this.btnConsultar.UseVisualStyleBackColor = true;
-            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
             // 
             // lblUF
             // 
@@ -308,13 +323,22 @@
             this.lblRua.TabIndex = 1;
             this.lblRua.Text = "Rua:";
             // 
+            // lblCEP
+            // 
+            this.lblCEP.AutoSize = true;
+            this.lblCEP.Location = new System.Drawing.Point(7, 205);
+            this.lblCEP.Name = "lblCEP";
+            this.lblCEP.Size = new System.Drawing.Size(38, 16);
+            this.lblCEP.TabIndex = 0;
+            this.lblCEP.Text = "CEP:";
+            // 
             // FormCadastroPaciente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 436);
+            this.ClientSize = new System.Drawing.Size(415, 456);
             this.Controls.Add(this.groupBox1);
-            this.MaximumSize = new System.Drawing.Size(431, 474);
+            this.MaximumSize = new System.Drawing.Size(431, 494);
             this.Name = "FormCadastroPaciente";
             this.Text = "Cadastro Paciente";
             this.groupBox1.ResumeLayout(false);
@@ -325,8 +349,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblCEP;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonCadastrar;
+        private System.Windows.Forms.DateTimePicker dtDataNasc;
+        private System.Windows.Forms.ComboBox selectTipoSang;
+        private System.Windows.Forms.MaskedTextBox mskdCPF;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textTelefone;
+        private System.Windows.Forms.TextBox textNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTipoSang;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblNome;
         private System.Windows.Forms.TextBox txtComp;
         private System.Windows.Forms.Label lblComp;
         private System.Windows.Forms.TextBox txtEstado;
@@ -339,17 +376,6 @@
         private System.Windows.Forms.Label lblCidade;
         private System.Windows.Forms.Label lblBairro;
         private System.Windows.Forms.Label lblRua;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblTipoSang;
-        private System.Windows.Forms.Label lblMail;
-        private System.Windows.Forms.Label lblTelefone;
-        private System.Windows.Forms.Label lblNome;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
-        private System.Windows.Forms.Label lblCPF;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblCEP;
     }
 }
